ba3f689c5f9ac31df33824a3943daa32d8efe8d0
Slav Basharov <slavbacharov@gmail.com>
2025-09-27 01:40:29 +0200

    fix: Removed windows-specific Cli

--- pvr/src/cli.rs
+++ pvr/src/cli.rs
@@ -2,42 +2,6 @@ use std::path::PathBuf;
 
 use clap::Parser;
 
-#[cfg(target_os = "windows")]
-#[derive(Parser)]
-#[command(version, author)]
-#[command(about = "A Rust port of UVR", long_about = None)]
-pub struct Cli {
-  #[arg(short, long, help = "Input audio file path")]
-  #[arg(value_name = "INPUT")]
-  pub input_path: PathBuf,
-
-  #[arg(short, long, help = "Directory to save output audio")]
-  #[arg(value_name = "OUTPUT", default_value = ".")]
-  pub output_path: PathBuf,
-
-  #[arg(
-    short,
-    long,
-    help = "The model used, leave blank to see all available models"
-  )]
-  #[arg(value_name = "PRESET")]
-  pub preset: Option<usize>,
-
-  #[arg(short, long, help = "Use DirectML backend for inference")]
-  pub directml_backend: bool,
-
-  #[arg(short, long, help = "Use CUDA backend for inference")]
-  pub cuda_backend: bool,
-
-  #[arg(short, long, help = "Use TensorRT backend for inference")]
-  pub tensorrt_backend: bool,
-
-  #[arg(short, long, help = "File format used to save results (wav/flac)")]
-  #[arg(value_name = "FORMAT", default_value = "flac")]
-  pub format: String,
-}
-
-#[cfg(not(target_os = "windows"))]
 #[derive(Parser)]
 #[command(version, author)]
 #[command(about = "A Rust port of UVR", long_about = None)]
@@ -65,6 +29,7 @@ pub struct Cli {
   #[arg(short = 'x', long, help = "ONNX runtime lib file path")]
   pub onnx_runtime_path: PathBuf,
 
+  #[cfg(target_os = "macos")]
   #[arg(short = 'c', long, help = "Use CoreML backend for inference")]
   pub coreml_backend: bool,
 
@@ -73,6 +38,10 @@ pub struct Cli {
 
   #[arg(short = 't', long, help = "Use TensorRT backend for inference")]
   pub tensorrt_backend: bool,
+
+  #[cfg(target_os = "windows")]
+  #[arg(short = 'd', long, help = "Use DirectML backend for inference")]
+  pub directml_backend: bool,
 
   #[arg(
     short = 'f',
